91d1ea012ffcff9e87807124ef14b18f520aec31
Min Ye Thu <min@Macbook.local>
2024-08-31 15:58:00 +1200

    Add Checkout class unit tests

---
+++ __tests__/checkout.test.ts
@@ -0,0 +1,71 @@
+import { afterEach, describe, expect, it } from '@jest/globals';
+import { mockItemRepository } from '../src/__mocks__/itemRepository';
+import Checkout from '../src/checkout';
+import PriceRules from '../src/priceRules';
+
+// Item   Unit      Special
+//        Price     Price
+//   --------------------------
+//     A     50       3 for 130
+//     B     30       2 for 45
+//     C     20
+//     D     15
+
+
+describe("Checkout test", () => {
+  const mockPriceRule = new PriceRules(mockItemRepository)
+  const checkout = new Checkout(mockPriceRule)
+
+  afterEach(() => {
+    checkout.clearAll()
+  })
+
+  it('should return total of 100 if A is scanned only twice', () => {
+    const items = ["A", "A"]
+    items.forEach(item => {
+      checkout.scan(item)
+    })
+    expect(checkout.total).toBe(100);
+  });
+
+  it('should return total of special price 130 if A is scanned 3 times', () => {
+    const items = ["A", "A", "A"]
+    items.forEach(item => {
+      checkout.scan(item)
+    })
+    expect(checkout.total).toBe(130);
+  });
+
+  it('should return total price of 260 if A is scanned 6 times', () => {
+    const items = ["A", "A", "A", "A"]
+    items.forEach(item => {
+      checkout.scan(item)
+    })
+    expect(checkout.total).toBe(180);
+  });
+
+  it('should return total price of 180 if A is scanned 4 times', () => {
+    const items = ["A", "A", "A", "A", "A", "A"]
+    items.forEach(item => {
+      checkout.scan(item)
+    })
+    expect(checkout.total).toBe(260);
+  });
+
+  it('should return total of 160 if A->A->B->A is scanned in the order', () => {
+    const items = ["A", "A", "B", "A"]
+    items.forEach(item => {
+      checkout.scan(item)
+    })
+    expect(checkout.total).toBe(160);
+  });
+
+  it('should return total of 170 if A->B->A->B->A is scanned in the order', () => {
+    const items = ["A", "B", "A", "B", "A"]
+    items.forEach(item => {
+      checkout.scan(item)
+    })
+    expect(checkout.total).toBe(175);
+  });
+  
+})
---
+++ src/__mocks__/itemRepository.ts
@@ -0,0 +1,59 @@
+// Item   Unit      Special
+//        Price     Price
+//   --------------------------
+//     A     50       3 for 130
+//     B     30       2 for 45
+//     C     20
+//     D     15
+
+import { IItemRepository } from "../interface/itemRepository"
+
+
+class MockItemRepository implements IItemRepository {
+    getUnitPriceInCent(item: string) {
+        switch(item){
+            case "A":
+                return 50
+            case "B":
+                return 30
+            case "C":
+                return 20
+            case "D":
+                return 10
+            default:
+                return 1
+        }
+    };
+
+    getPromotionalUnit(item: string) {
+        switch(item){
+            case "A":
+                return 3
+            case "B":
+                return 2
+            case "C":
+                return 0
+            case "D":
+                return 0
+            default:
+                return 0
+        }
+    };
+    getPromotionalPriceInCent(item: string) {
+        switch(item){
+            case "A":
+                return 130
+            case "B":
+                return 45
+            case "C":
+                return 0
+            case "D":
+                return 0
+            default:
+                return 0
+        }
+    };
+
+}
+
+export const mockItemRepository = new MockItemRepository()
